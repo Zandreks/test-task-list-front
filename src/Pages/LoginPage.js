f484f7a1373a17767541daa3bb75fa8f8e17510d
Maxim Severyanov <zandreks@gmail.com>
2021-05-30 04:55:53 +0600

    edit login page

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,79 +1,74 @@
 import React from "react";
-import {Form, Input, Button} from 'antd';
+import { Form, Input, Button } from "antd";
 import User from "../Controllers/User";
 import { useHistory } from "react-router-dom";
 
 const layout = {
-    labelCol: {
-        span: 7,
-    },
-    wrapperCol: {
-        span: 12,
-    },
-}
+  labelCol: {
+    span: 7,
+  },
+  wrapperCol: {
+    span: 12,
+  },
+};
 const tailLayout = {
-    wrapperCol: {
-        offset: 8,
-        span: 16,
-    },
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
 };
 
-const LoginPage = ()=>{
-    let history = useHistory()
+const LoginPage = () => {
+  let history = useHistory();
 
-    const onFinish = async (values) => {
-        const formData = new FormData()
-        formData.append("username",values.username)
-        formData.append("password",values.password)
-        const userController = new User()
-        let res = await userController.signIn(formData)
-        if (!!res) {
-            form.resetFields();
-            localStorage.setItem("key",res.token)
-            history.push("/");
-        }
-    };
-    const [form] = Form.useForm();
+  const onFinish = async (values) => {
+    const formData = new FormData();
+    formData.append("username", values.username);
+    formData.append("password", values.password);
+    const userController = new User();
+    let res = await userController.signIn(formData);
+    if (!!res) {
+      form.resetFields();
+      localStorage.setItem("key", res.token);
+      history.push("/");
+    }
+  };
+  const [form] = Form.useForm();
 
-    return(
-        <div>
-            <Form
-                {...layout}
-                name="addTask"
-                form={form}
-                onFinish={onFinish}
-            >
-                <Form.Item
-                    label="Имя пользователя"
-                    name="username"
-                    rules={[
-                        {
-                            required: true
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
+  return (
+    <div>
+      <Form {...layout} name="addTask" form={form} onFinish={onFinish}>
+        <Form.Item
+          label="Имя пользователя"
+          name="username"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-                <Form.Item
-                    label="Пароль"
-                    name="password"
-                    rules={[
-                        {
-                            required: true
-                        },
-                    ]}
-                >
-                    <Input.Password />
-                </Form.Item>
+        <Form.Item
+          label="Пароль"
+          name="password"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-                <Form.Item {...tailLayout}>
-                    <Button type="primary" htmlType="submit">
-                       Войти
-                    </Button>
-                </Form.Item>
-            </Form>
-        </div>
-    )
-}
-export default LoginPage
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Войти
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+export default LoginPage;
